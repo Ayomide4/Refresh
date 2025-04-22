097f20a8523a8815f33cc0c9220a15f0f89c644c
Ayomide Omotosho <aomotosho4@gmail.com>
2025-04-22 15:59:50 -0500

    add basic seo, audit fix force

---
+++ web/next-sitemap.config.js
@@ -0,0 +1,10 @@
+
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://myrefresh.org',
+  generateRobotsTxt: true, // It’ll generate robots.txt too
+  changefreq: 'weekly',
+  priority: 0.7,
+};
+
+export default config
