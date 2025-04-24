7a7e0d0011d46dbdc00db4e75e40729646d94e76
Ayomide Omotosho <aomotosho4@gmail.com>
2025-04-24 13:23:04 -0500

    remove text and overlay

--- web/app/components/EventCard.tsx
+++ web/app/components/EventCard.tsx
@@ -16,18 +16,10 @@ export const EventCard = ({ event, imageUrl }: EventCardProps) => {
         height={320}
         className="object-cover rounded-2xl"
       />
-      <div
-        className="absolute bottom-0 left-0 w-full h-32"
-        style={{
-          background: "linear-gradient(to top, black 5%, transparent 90%)",
-        }}
-      ></div>
+
       <Link href="https://www.youtube.com/@therefreshgathering/videos" target="_blank" rel="noopener noreferrer" className="absolute top-5 right-5 bg-black rounded-full text-sm text-white px-4 py-2 cursor-pointer">
         Watch video
       </Link>
-      <h2 className="absolute bottom-5 left-5 w-full text-white text-left p-2 text-xl font-semibold">
-        {event.title}
-      </h2>
     </article>
   );
 };
